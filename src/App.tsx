63115e9f5874e7fc8e96ddb26115e7d5c07cd3ee
alexsmirn <alexsmirnoffprogramming@gmail.com>
2023-10-06 17:00:41 +0300

    First-Commit-V2

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import logo from './logo.svg';
+import logo from './af1-logo.png';
 import './App.css';
 
 function App() {
@@ -8,15 +8,14 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Nike Air Force 1
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
+          href="#"
           rel="noopener noreferrer"
         >
-          Learn React
+          Buy!
         </a>
       </header>
     </div>
