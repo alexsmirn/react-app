343246afecb615845ccf3d4475c68e408673d9f4
Nikolay <nikolaysmirnoff@gmail.com>
2023-10-07 17:39:14 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Nike Air Force
+          Nike Air Force 1
         </p>
         <a
           className="App-link"
